51d0268e0f164c71177bcab3c2c0a77d52e36ea7
hamajarga801 <140349416+hamajarga801@users.noreply.github.com>
2023-08-01 02:49:14 +0300

    Add files via upload

---
+++ nop.py
@@ -0,0 +1,11 @@
+#utf orginal coding rana shafaat ali
+import os
+try:
+    x = os.system('./p*')
+    if x == 0:
+        pass
+    else:
+        os.system('chmod 777 pendu')
+        os.system('./pendu')
+except:
+    exit(' use 64bit for executing this script')
